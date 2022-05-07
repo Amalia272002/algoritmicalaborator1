1849e1a4cdbdc133cfe8e7cbc5a375788a421606
user <user@MSI>
2022-05-07 17:46:29 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace algoritmica_laborator1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
